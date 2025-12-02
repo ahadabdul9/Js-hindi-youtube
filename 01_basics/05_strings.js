96f882a9cb9b9b0c5e695c63b3ff8352d59bd87a
Abdul Ahad <151631013+ahadabdul9@users.noreply.github.com>
2025-12-02 07:14:31 +0000

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,6 +1,33 @@
 const name = "ahad"
 const repoCount = 50
 
-console.log(name + repoCount + " value");
+// console.log(name + repoCount + " value");
 
-console.log('Hello my name is ${name} and my repo count ${repoCount}');
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Ahadhcbs')
+console.log(gameName.length);
+console.log(gameName[0]);
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-6,3)
+console.log(anotherString);
+
+const newStringOne = "    ahad   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudary"
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('sunny'));
+
+console.log(gameName.split('-'));
+
+
+
+
+
+
